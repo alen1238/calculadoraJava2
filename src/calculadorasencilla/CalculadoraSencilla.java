c9beb21b574fb2c01fe1f210829dbfcbccd546b4
alfonso lara <orozcoenrique159@gmail.com>
2024-08-19 19:54:24 -0500

    primer commit en calculadora

---
+++ src/calculadorasencilla/CalculadoraSencilla.java
@@ -0,0 +1,9 @@
+package calculadorasencilla;
+
+public class CalculadoraSencilla {
+
+    public static void main(String[] args) {
+        System.out.println("Esta es una muestra");
+    }
+    
+}
